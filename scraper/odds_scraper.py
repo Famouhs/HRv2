03c72ad5df152548360d09057e7147238fde7769
Famouhs <Alvarez.Jeanp@gmail.com>
2025-04-19 11:16:21 -0400

    Update odds_scraper.py

--- scraper/odds_scraper.py
+++ scraper/odds_scraper.py
@@ -1,8 +1,15 @@
-
 import pandas as pd
 
 def get_hr_odds(date):
     return pd.DataFrame([
         {"Player": "Fernando Tatis Jr.", "HR Odds": "+255"},
-        {"Player": "Peter Alonso", "HR Odds": "+310"}
+        {"Player": "Peter Alonso", "HR Odds": "+310"},
+        {"Player": "Tyler Soderstrom", "HR Odds": "+390"},
+        {"Player": "James Wood", "HR Odds": "+500"},
+        {"Player": "Spencer Torkelson", "HR Odds": "+560"},
+        {"Player": "Corbin Carroll", "HR Odds": "+400"},
+        {"Player": "Eugenio Suárez", "HR Odds": "+430"},
+        {"Player": "Juan Soto", "HR Odds": "+430"},
+        {"Player": "Yordan Alvarez", "HR Odds": "+330"},
+        {"Player": "Wilmer Flores", "HR Odds": "+680"}
     ])
